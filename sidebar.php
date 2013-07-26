a660a9eb80f93dd6c0aec61f3a4f5e99bf17335b
psmagicman <julienlaw1@gmail.com>
2013-07-26 09:47:37 -0700

    renamed right sidebar to primary sidebar since it can be let or right

--- sidebar.php
+++ sidebar.php
@@ -14,7 +14,7 @@
 
 <aside class="large-3 columns sidebar">
 
-<?php if ( dynamic_sidebar('Sidebar Right') ) : elseif( current_user_can( 'edit_theme_options' ) ) : ?>
+<?php if ( dynamic_sidebar('Primary Sidebar') ) : elseif( current_user_can( 'edit_theme_options' ) ) : ?>
 
 	<h5><?php _e( 'No widgets found.', 'foundaton' ); ?></h5>
 	<p><?php printf( __( 'It seems you don\'t have any widgets in your sidebar! Would you like to %s now?', 'foundation' ), '<a href=" '. get_admin_url( '', 'widgets.php' ) .' ">populate your sidebar</a>' ); ?></p>
